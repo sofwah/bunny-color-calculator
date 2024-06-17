571a07bfc29cde664db31ec8e9d50d823309b125
sofwah <sofia.wahlmark.0513@student.lu.se>
2024-06-17 12:18:51 +0200

    Add documentation

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,14 @@
-const defaultGeneSelectValues = { "a": ["_", "_"], "b": ["_", "_"], "c": ["_", "_"], "d": ["_", "_"], "g": ["_", "_"] };
+// This is used to reset the gene pair drop-downs
+const defaultGeneSelectValues = {
+  "a": ["_", "_"],
+  "b": ["_", "_"],
+  "c": ["_", "_"],
+  "d": ["_", "_"],
+  "g": ["_", "_"]
+};
 
+// This describes the order of dominance within each locus.
+// E.g. A covers achi, achi covers am, and so on.
 const dominanceOrder = {
   "a": ["A", "achi", "am", "an", "a", "_"],
   "b": ["Be", "B", "bj", "b", "_"],
@@ -8,6 +17,7 @@ const dominanceOrder = {
   "g": ["G", "go", "g", "_"],
 };
 
+// A list of the available loci
 const lociList = Object.keys(dominanceOrder);
 
 export {
