98183cbe2000aecb4177012a9d43600326a2bb08
sofwah <sofia.wahlmark.0513@student.lu.se>
2024-06-17 16:32:36 +0200

    Shorten lines that are too long

--- src/GenePairSelect.js
+++ src/GenePairSelect.js
@@ -6,7 +6,11 @@ export function GenePairSelect({ locus, selectedGeneList, handleSelectorChange }
 
   function createGeneSelect(idx) {
     return (
-      <select value={genePair[idx]} onChange={(e) => handleSelectorChange(locus, idx, e.target.value)} className="gene-select mx-px rounded-sm">
+      <select
+        value={genePair[idx]}
+        onChange={(e) => handleSelectorChange(locus, idx, e.target.value)}
+        className="gene-select mx-px rounded-sm"
+      >
         {optionList.map(optionStr => {
           return (
             <option key={crypto.randomUUID()} value={optionStr}>{optionStr}</option>
