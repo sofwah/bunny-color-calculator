ddfe1ca1f909aedf8d71079657e6b3c56201857c
sofwah <sofia.wahlmark.0513@student.lu.se>
2024-06-18 17:22:07 +0200

    Add dictionary to format exponents in html and coorect apostrophes

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,27 +1,37 @@
 // This is used to reset the gene pair drop-downs
 const DEFAULT_GENE_SELECT_VALUES = {
-  "a": ["_", "_"],
-  "b": ["_", "_"],
-  "c": ["_", "_"],
-  "d": ["_", "_"],
-  "g": ["_", "_"]
+  'a': ['_', '_'],
+  'b': ['_', '_'],
+  'c': ['_', '_'],
+  'd': ['_', '_'],
+  'g': ['_', '_']
 };
 
 // This describes the order of dominance within each locus.
 // E.g. A covers achi, achi covers am, and so on.
 const DOMINANCE_ORDER = {
-  "a": ["A", "achi", "am", "an", "a", "_"],
-  "b": ["Be", "B", "bj", "b", "_"],
-  "c": ["C", "c", "_"],
-  "d": ["D", "d", "_"],
-  "g": ["G", "go", "g", "_"],
+  'a': ['A', 'achi', 'am', 'an', 'a', '_'],
+  'b': ['Be', 'B', 'bj', 'b', '_'],
+  'c': ['C', 'c', '_'],
+  'd': ['D', 'd', '_'],
+  'g': ['G', 'go', 'g', '_'],
 };
 
 // A list of the available loci
 const LOCUS_LIST = Object.keys(DOMINANCE_ORDER);
 
+const HTML_GENE_DICT = {
+  'achi': 'a<sup>chi</sup>',
+  'am': 'a<sup>m</sup>',
+  'an': 'a<sup>n</sup>',
+  'Be': 'B<sup>e</sup>',
+  'bj': 'b<sup>j</sup>',
+  'go': 'g<sup>o</sup>'
+}
+
 export {
   DEFAULT_GENE_SELECT_VALUES,
   DOMINANCE_ORDER,
-  LOCUS_LIST
+  LOCUS_LIST,
+  HTML_GENE_DICT
 }
