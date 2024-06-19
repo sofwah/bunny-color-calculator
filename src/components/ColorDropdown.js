5a1df4f0dc3b13f250e3fd1d15f720ef45b51082
sofwah <sofia.wahlmark.0513@student.lu.se>
2024-06-19 12:13:31 +0200

    Small color changes to make gene codes clearer

--- src/components/ColorDropdown.js
+++ src/components/ColorDropdown.js
@@ -35,13 +35,13 @@ export function ColorDropdown({ label, content }) {
         onClick={toggleDropdown}
         className="cursor-pointer flex items-center py-1 shadow-[inset_0_-8px_1px_-8px_rgba(0,0,0,0.2)]"
       >
-        <ArrowIcon isOpen={isOpen} /> {}
+        <ArrowIcon isOpen={isOpen} />
         <span
           className="ml-2 font-medium tracking-wide" dangerouslySetInnerHTML={{ __html: label }}
         ></span>
       </div>
       {isOpen && (
-        <div className="pl-5 py-1 pb-2 text-sm text-gray-800 tracking-wide bg-[rgba(0,0,0,0.02)]">
+        <div className="pl-5 py-1 pb-2 text-sm text-gray-900 tracking-wide bg-[rgba(0,0,0,0.01)]">
           {content.map((item, index) => (
             <div key={index} dangerouslySetInnerHTML={{ __html: item }}></div>
           ))}
